554ae94e12e2cddc8e45cce4046956c55c7bd5e2
Tomas Plevko <tplevko@redhat.com>
2025-06-11 15:22:41 +0200

    Add enum, properties and oneOff fields

--- src/providers/FormComponentFactoryProvider.tsx
+++ src/providers/FormComponentFactoryProvider.tsx
@@ -8,6 +8,10 @@ import { DisabledField } from '../fields/DisabledField';
 import { BooleanField } from '../fields/BooleanField';
 import { ObjectField } from '../fields/ObjectField/ObjectField';
 import { ArrayField } from '../fields/ArrayField/ArrayField';
+import { EnumField } from '../fields/EnumField';
+import { PropertiesField } from '../fields/PropertiesField/PropertiesField';
+import { OneOfField } from '../fields/OneOfField/OneOfField';
+import { AllOfField } from '../fields/ObjectField/AllOfField';
 
 type FormComponentFactoryContextValue = (schema: KaotoSchemaDefinition['schema']) => FunctionComponent<FieldProps>;
 
@@ -22,6 +26,18 @@ export const FormComponentFactoryProvider: FunctionComponent<PropsWithChildren> 
       return PasswordField;
     } else if (schema.type === 'string' && schema.title && TextAreaPropertyNames.includes(schema.title)) {
       return TextAreaField;
+    } else if (schema.type === 'string' && Array.isArray(schema.enum)) {
+      return EnumField;
+    } else if (schema.type === 'object' && Object.keys(schema?.properties ?? {}).length === 0) {
+      /*
+       * If the object has no properties, we render a generic key-value pairs field
+       * This is useful for langchain4j-tools consumer components or when configuring beans entities
+       */
+      return PropertiesField;
+    // } else if (schema.type === 'string' && schema.format?.startsWith('bean:')) {
+    //   return BeanField;
+    // } else if (schema.format === 'expression' || schema.format === 'expressionProperty') {
+    //   return ExpressionField;
     }
 
     switch (schema.type) {
@@ -35,6 +51,14 @@ export const FormComponentFactoryProvider: FunctionComponent<PropsWithChildren> 
         return ObjectField;
       case 'array':
         return ArrayField;
+    }
+
+    if (Array.isArray(schema.oneOf)) {
+      return OneOfField;
+    } else if (Array.isArray(schema.allOf)) {
+      return AllOfField;
+    } else if (Array.isArray(schema.anyOf)) {
+      throw new Error('FormComponentFactoryProvider: AnyOf should be handled in the scope of the ObjectField');
     }
 
     return DisabledField;
